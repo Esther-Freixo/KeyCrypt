106fa329ea123b3e11e056fefc6a32aa1d306111
Esther Freixo <freixo.esther@gmail.com>
2025-04-27 19:10:03 -0300

    feat: add open file feature

--- keycrypt.c
+++ keycrypt.c
@@ -9,11 +9,17 @@ int main() {
     scanf("%5s", userPassword);
 
     if (strcmp(password, userPassword) == 0){
-        printf("Senha confirmada");
+        FILE *file = fopen("text.dat", "rb");
+        if (file == NULL) {
+            printf("Falha ao abrir o arquivo");
+            return 1;
+        }
+
+        printf("Arquivo aberto com sucesso!");
+        fclose(file);
         return 0;
     } else {
         printf("Senha incorreta");
         return 1;
     }
-    return 0;
 }
